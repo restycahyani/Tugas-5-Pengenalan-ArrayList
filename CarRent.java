f5d78ddf14f26b7812cbe8c6022982e0a3ea7b16
restycahyani <restycahyani227@student.ub.ac.id>
2022-06-18 15:28:07 +0700

    first

---
+++ CarRent.java
@@ -0,0 +1,23 @@
+package com.car;
+
+public class CarRent {
+    public CarRider rider;
+    public Car car;
+    public int rentDur;
+
+    CarRent(CarRider rider, Car car, int rentDur) {
+        this.rider = rider;
+        this.car = car;
+        this.rentDur = rentDur;
+    }
+
+    public CarRider getRider() {
+        return rider;
+    }
+    public Car getCar() {
+        return car;
+    }
+    public int getRentDur() {
+        return rentDur;
+    }
+}
---
+++ CarRider.java
@@ -0,0 +1,22 @@
+package com.car;
+
+public class CarRider {
+    public String name, phone;
+    public int age;
+
+    CarRider(String name, int age, String phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getPhone() {
+        return phone;
+    }
+}
